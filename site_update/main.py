62a3763cba08b30e830de794476f9dfadbebf007
moonz <mihail.moonz@gmail.com>
2019-06-01 13:27:03 +0300

    подготовка

--- site_update/main.py
+++ site_update/main.py
@@ -1,9 +1,20 @@
-"""
-Основной функционал для обработки REST API wp
-
-"""
 import requests, json
 from bs4 import BeautifulSoup as bs
+
+
+def get_token(token_pach: str) -> str:
+    """
+    Читает текстоый файл с ключом от REST API
+    :param token_pach: str = путь к файлу .txt
+    :return: str = возвращает строку с токеном.
+    """
+    with open(token_pach, 'r') as file:
+        token = file.read()
+
+    return token
+
+
+TOKEN = get_token('token.txt')
 
 
 def get_request(url: str) -> str:  # return html
@@ -15,20 +26,21 @@ def get_request(url: str) -> str:  # return html
     session = requests.Session()
     header = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 '
-                      '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+                      '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+        'Authorization': TOKEN}
     try:
         html = session.get(url, headers=header)
         if html.status_code == 200:
             return html.text
 
         else:
-            return f'error status_code = {html.status_code}'
+            return f'error, status_code = ({html.status_code})'
 
     except Exception as error:
         return str(error)
 
 
-def post_request(value: dict, url: str) -> str:
+def post_request(value: dict, url: str) -> str:  # return status_code
     """
     Функция служит для отправик POST запроса на REST API сайта.
     :param value: dict = состоит из сео параметров и исходного кода страницы.
@@ -37,10 +49,13 @@ def post_request(value: dict, url: str) -> str:
     """
     session = requests.Session()
     header = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 '
-                      '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        'Content-Type': 'application/JSON',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 ('
+                      'KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+        'Authorization': TOKEN}
 
     data = json.dumps(value)
+
     try:
         post = session.post(url, data=data, headers=header)
         return str(post.status_code)
@@ -49,12 +64,13 @@ def post_request(value: dict, url: str) -> str:
         return str(error)
 
 
-def get_price(url: str) -> dict:
+def get_price() -> dict:
     """
     Служит для получения актуальнго прайса цена на цветные металлы.
     :param url: ссылка на страницу https://metal52.ru/prinimaem/cvetnoj-lom/
     :return: dict = словарь из название типов металла и его цены | {'Медь':320}
     """
+    url = 'https://metal52.ru/prinimaem/cvetnoj-lom/'
     soup = bs(get_request(url), 'html.parser')
 
     a = soup.find('table', class_='tftable')
@@ -76,11 +92,41 @@ def get_price(url: str) -> dict:
     return data
 
 
-def color_metal_changer(value: dict) -> str:
-    pass
+def color_metal_changer(value: dict, data: list) -> str:
+    """
+    Ожидается :param data = list = список со списком названий которые нужно изменить | [[Медь:320][Латунь:120]]
+    Обновляет цены на странице с цветным ломам
+    :param value: dict = словарь с наименованием металлов и цен.
+    :return: str = статус успеха операции | возвращается из post_request
+    """
+    for metal in data:
+        price_data = metal[0].split(':')  # 0 = Название металла | 1 = Новая цена
+        if value.get(price_data[0]) != price_data[1]:
+            value.update({price_data[0]: price_data[1]})
 
-def med_changer(value: str) -> str:
-    pass
+    # url = 'https://metal52.ru/prinimaem/cvetnoj-lom/'
+    # status = post_request(value, url)
+    return value
 
-test = get_price('https://metal52.ru/prinimaem/cvetnoj-lom/')
+
+def med_changer(value: int) -> str:
+    """
+    Обновляет цену и сео заголову на Медь
+    :param value: int = новая цена на медь.
+    :return: str = статус успеха операции | возвращается из post_request
+    """
+
+
+def accumulator_changer(value: int) -> str:
+    """
+    Обновляет цену и сео заголову на Акб
+    :param value: int = новая цена на аккомуляторы.
+    :return: str = статус успеха операции | возвращается из post_request
+    """
+
+
+test_data = [['Медь:342'], ['Алюминий:56']]
+test = color_metal_changer(get_price(), test_data)
 print(test)
+
+# post https://metal52.ru/wp-json/wp/v2/pages/1004
--- yandex_handbook/main.py
+++ yandex_handbook/main.py
@@ -3,39 +3,45 @@
 Файл будет содержать актуальные цены на лома.
 """
 
-import xlrd  # Чтение
-
-# rb = xlrd.open_workbook('price-list-template.xls', formatting_info=True)
-# sheet = rb.sheet_by_index(0)
+# import xlrd  # Чтение
 #
-# for rownum in range(sheet.nrows):
-#     row = sheet.row_values(rownum)
-#     for c_el in row:
-#         print(c_el)
+# # rb = xlrd.open_workbook('price-list-template.xls', formatting_info=True)
+# # sheet = rb.sheet_by_index(0)
+# #
+# # for rownum in range(sheet.nrows):
+# #     row = sheet.row_values(rownum)
+# #     for c_el in row:
+# #         print(c_el)
+#
+#
+# import xlwt  # Запись
+#
+# """
+# 0 = Категория |1 = Название |2 = Описание |3 = Цена|4 = Фото |
+#
+# Словарь со списокм значений, ключ словаря это номер строки, индес списка = столбцу
+# data = {0 : [Цветной лом, Медь, лбая медная деталь, 330р кг, vk.com/sdf23d.jpg
+# """
+#
+# wb = xlwt.Workbook()
+# ws = wb.add_sheet('A Test Sheet')
+#
+#
+#
+#
+# ws.write(0, 0, 'Test')
+# ws.write(0, 1, 'Test')
+# ws.write(0, 2, 'Test')
+# # ws.write(1, 0, datetime.now(), style1)
+# # ws.write(2, 0, 1)
+# # ws.write(2, 1, 1)
+# # ws.write(2, 2, xlwt.Formula("A3+B3"))
+#
+#
+# wb.save('example.xls', )
 
+try:
+    1 / 0
 
-import xlwt  # Запись
-
-"""
-0 = Категория |1 = Название |2 = Описание |3 = Цена|4 = Фото | 
-
-Словарь со списокм значений, ключ словаря это номер строки, индес списка = столбцу
-data = {0 : [Цветной лом, Медь, лбая медная деталь, 330р кг, vk.com/sdf23d.jpg
-"""
-
-wb = xlwt.Workbook()
-ws = wb.add_sheet('A Test Sheet')
-
-
-
-
-ws.write(0, 0, 'Test')
-ws.write(0, 1, 'Test')
-ws.write(0, 2, 'Test')
-# ws.write(1, 0, datetime.now(), style1)
-# ws.write(2, 0, 1)
-# ws.write(2, 1, 1)
-# ws.write(2, 2, xlwt.Formula("A3+B3"))
-
-
-wb.save('example.xls', )
+except Exception as error:
+    print(type(error))
